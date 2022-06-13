b8dc632086a1e5b84792823956e4e23756d3f7ab
Michael <semenov.yuri.michael@gmail.com>
2022-06-14 00:05:22 +0300

    Working

---
+++ src/main/java/ru/jhecks/AppInit.java
@@ -0,0 +1,47 @@
+package ru.jhecks;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import ru.jhecks.config.WebConfig;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerEncodingFilter(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerEncodingFilter(ServletContext aContext) {
+        FilterRegistration.Dynamic encodingFilter = aContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+}
---
+++ src/main/java/ru/jhecks/service/UserService.java
@@ -0,0 +1,15 @@
+package ru.jhecks.service;
+
+import ru.jhecks.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void createUser(User user);
+    void updateUser(User user);
+    void updateUserById(long id, User user);
+    void deleteUser(User user);
+    void deleteUserById(long id);
+    User getUser(long id);
+    List<User> getAllUsers();
+}
